7832b0bbd6eeeb44fe66e4e610d5dfffe2e9a553
Alejandro-Cuesta <janittoss@hotmail.com>
2025-08-12 11:15:11 +0200

    test:primer test pasado (addMoment & getAllMoments)

--- src/main/java/dev/alejandro/insideout/service/Diary.java
+++ src/main/java/dev/alejandro/insideout/service/Diary.java
@@ -4,36 +4,35 @@ import dev.alejandro.insideout.model.Moment;
 import dev.alejandro.insideout.model.Emotion;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
  //Aquí irá toda la lógica: añadir, eliminar, listar y filtrar momentos//
 public class Diary {
 
-    public Diary() { //Constructor
+    private List<Moment> moments; //lista interna para guardar momentos
 
+    public Diary() { //Constructor
+        moments = new ArrayList<>();
     }
 
     public void addMoment(Moment moment) { //Añadir momento//
-
+        moments.add(moment);
     }
     
-    public List<Moment> getallMoments() { //Devolveer todos los momentos//
-
-        return null;
+    public List<Moment> getAllMoments() { //Devolveer todos los momentos//
+        return moments;
     }
 
     public boolean removeMomentById(int id) { //Eliminar metodo po Id//
-
         return false;
     }
 
     public List<Moment> filterByEmotion(Emotion emotion) { //Devolver metodos que coincidad con una emocion//
-
         return null; 
     }
 
     public List<Moment> filterByDate(LocalDate date) { //Devolver metodos que coincidan con la fecha//
-
         return null;
     }
 }
