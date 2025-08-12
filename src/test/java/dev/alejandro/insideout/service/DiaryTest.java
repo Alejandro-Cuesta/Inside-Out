f89ecfa82cc0b9931624f13ad0dde415eba73704
Alejandro-Cuesta <janittoss@hotmail.com>
2025-08-12 12:22:48 +0200

    test:añadido tercer test filterByEmotion

--- src/test/java/dev/alejandro/insideout/service/DiaryTest.java
+++ src/test/java/dev/alejandro/insideout/service/DiaryTest.java
@@ -43,7 +43,7 @@ public class DiaryTest {
     }
 
     @Test
-    public void testRemoveMomentById() {
+    public void testRemoveMomentById() { //eliminar un momento por Id//
     Moment moment = new Moment();
     moment.setId(1);
     moment.setTitle("Un dia en el parque de atracciones");
@@ -61,5 +61,34 @@ public class DiaryTest {
     assertTrue(diary.getAllMoments().isEmpty(), "La lista de momentos debería estar vacía después de eliminar");
     }
 
+    @Test
+    void testFilterByEmotion() { //filtrar momento por Emocion//
+    Moment moment1 = new Moment();
+    moment1.setId(1);
+    moment1.setTitle("Un día en el parque de atracciones");
+    moment1.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed eros vel massa scelerisque convallis interdum ut purus.");
+    moment1.setEmotion(Emotion.ALEGRIA);
+    moment1.setMomentDate(LocalDate.of(2024, 5, 1));
+    moment1.setCreationDate(LocalDateTime.now());
+    moment1.setModificationDate(LocalDateTime.now());
+
+    Moment moment2 = new Moment();
+    moment2.setId(2);
+    moment2.setTitle("Una tarde en el cementerio");
+    moment2.setDescription("Pensando en los que ya no estan");
+    moment2.setEmotion(Emotion.TRISTEZA);
+    moment2.setMomentDate(LocalDate.of(2024, 4, 17));
+    moment2.setCreationDate(LocalDateTime.now());
+    moment2.setModificationDate(LocalDateTime.now());
+
+    diary.addMoment(moment1);
+    diary.addMoment(moment2);
+
+    List<Moment> filtered = diary.filterByEmotion(Emotion.ALEGRIA);
+
+    assertNotNull(filtered, "La lista filtrada no debería ser nula");
+    assertEquals(1, filtered.size(), "Debería haber un solo momento con la emoción ALEGRIA");
+    assertEquals("Un día en el parque de atracciones", filtered.get(0).getTitle(), "El título debería coincidir con el momento feliz");
+    }
 
 }
